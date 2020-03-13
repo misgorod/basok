22d8cd2ecb392e16c67f66c2bad87a2b56634fa7
Andrey Bulgaru <agbulgaru@avito.ru>
2020-03-13 23:12:51 +0300

    init

---
+++ lab1/basok.py
@@ -0,0 +1,29 @@
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtCore import pyqtSlot
+from design import Ui_MainWindow
+import sys
+from counter import count_repeated, InvalidLengthException
+            
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, obj=None, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+        self.button.clicked.connect(self.on_click)
+
+    @pyqtSlot()
+    def on_click(self):
+        text = self.sourceText.toPlainText()
+        try:
+            self.targetText.setPlainText(count_repeated(text))
+        except InvalidLengthException:
+            self.targetText.setPlainText("длина строки не должна превышать 30 символов")
+ 
+def main():
+    app = QtWidgets.QApplication([])
+    win = MainWindow()
+    win.show()
+    sys.exit(app.exec())
+
+if __name__ == '__main__':
+    main()
